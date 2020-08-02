bc1e00ee2cd3a2534ce1f7ec430fc2a26ea7c549
Artback <artback@protonmail.com>
2020-08-02 17:45:52 +0200

    Refactor stops query function

--- query/query.go
+++ query/query.go
@@ -37,9 +37,9 @@ func (r *Repository) Connect(c config.DatabaseConfiguration) error {
 		passwordArg = "password=" + pass
 	}
 	var err error
-	db_string := fmt.Sprintf("host=%s port=%d user=%s %s dbname=%s sslmode=disable",
+	dbString := fmt.Sprintf("host=%s port=%d user=%s %s dbname=%s sslmode=disable",
 		host, port, user, passwordArg, db)
-	r.Db, err = sql.Open(c.Driver, db_string)
+	r.Db, err = sql.Open(c.Driver, dbString)
 	if err != nil {
 		return err
 	}
@@ -47,19 +47,25 @@ func (r *Repository) Connect(c config.DatabaseConfiguration) error {
 	return r.Db.Ping()
 }
 
-func NewConnectedRepository(c config.DatabaseConfiguration) (*Repository, error) {
+func NewConnected(c config.DatabaseConfiguration) (*Repository, error) {
 	repo := new(Repository)
 	err := repo.Connect(c)
 	return repo, err
 }
 
-func (r *Repository) GetStops(lat string, lon string, radius string, maxstops string) (*sql.Rows, error) {
-	return r.Db.Query(
-		fmt.Sprintf("SELECT s.stop_id as id, arrival_time, departure_time, stop_name as name, stop_lat as lat, stop_lon as lon,"+
-			" trip_headsign as headsign, date, (date::varchar || ' ' || departure_time) as date_string"+
-			" from stop_times JOIN stops s ON s.stop_id = stop_times.stop_id"+
-			" JOIN trips t on stop_times.trip_id = t.trip_id JOIN calendar_dates cd on t.service_id = cd.service_id"+
-			" WHERE s.stop_id in (select distinct stop_id from stops where st_dwithin(geography(st_point(stop_lat, stop_lon)), geography(st_point(%s,%s)), %s)"+
-			" order by stop_id limit %s)  AND ((date(current_timestamp + interval '- 4 hours') = cd.date"+
-			" OR date(current_timestamp + interval '20 hours') = cd.date)) order by date_string;", lat, lon, radius, maxstops))
+func (r *Repository) GetStops(la float64, lo float64, radius int, maxStops int) (*sql.Rows, error) {
+	return r.Db.Query(stopsQuery(la, lo, radius, maxStops))
+}
+func stopsQuery(la float64, lo float64, radius int, maxStops int) string {
+	lat := strconv.FormatFloat(la, 'f', -1, 64)
+	lon := strconv.FormatFloat(lo, 'f', -1, 64)
+	r := strconv.Itoa(radius)
+	m := strconv.Itoa(maxStops)
+	return fmt.Sprintf(`SELECT s.stop_id as id, arrival_time, departure_time, stop_name as name, stop_lat as lat, stop_lon as lon,
+			 trip_headsign as headsign, date, (date::varchar || ' ' || departure_time) as date_string
+			 from stop_times JOIN stops s ON s.stop_id = stop_times.stop_id
+			 JOIN trips t on stop_times.trip_id = t.trip_id JOIN calendar_dates cd on t.service_id = cd.service_id
+			 WHERE s.stop_id in (select distinct stop_id from stops where st_dwithin(geography(st_point(stop_lat, stop_lon)), geography(st_point(%s,%s)), %s)
+			 order by stop_id limit %s)  AND ((date(current_timestamp + interval '- 4 hours') = cd.date
+			 OR date(current_timestamp + interval '20 hours') = cd.date)) order by date_string;`, lat, lon, r, m)
 }
